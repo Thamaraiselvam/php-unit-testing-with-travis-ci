7e96066a9b7cf97457929ed97dbf6491ebbd0feb
thamaraiselvam <thamaraiselvam@live.com>
2017-06-24 23:33:22 +0530

    travis.yml syntax error fixed

--- Test.php
+++ Test.php
@@ -1,4 +1,10 @@
 <?php
+
+// backward compatibility
+if (!class_exists('\PHPUnit\Framework\TestCase')) {
+    class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
+}
+
 Class Test extends PHPUnit_Framework_TestCase{
 	public function test_sample_case(){
 		$this->assertEquals(1+1,2);
